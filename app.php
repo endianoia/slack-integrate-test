a7b0cebedb517123d96d66f0e52ebbbd00a50562
ryuichi <wasuremono.7z@gmail.com>
2016-09-19 11:40:03 +0900

    update app.php

--- app.php
+++ app.php
@@ -1,14 +1,27 @@
 <?php
+
+if ($_POST) {
+    $text = $_POST['text'];
+    $text_arr = preg_split('/[\s]+/', $text, 4);
+
+    $addTo_arr = preg_split('/[\|]+/', $text_arr[1]);
+    $addTo = substr($addTo_arr[1], 0, -1);
+    $setSubject = $text_arr[2];
+    $setText = $text_arr[3];
+
     require 'vendor/autoload.php';
+
     $sendgrid = new SendGrid(getenv('SENDGRID_USERNAME'), getenv('SENDGRID_PASSWORD'));
 
     $mail = new SendGrid\Email();
     $mail
-        ->addTo('wasuremono.7z@gmail.com')
-        ->setFrom('wasuremono.7z@gmail.com')
-        ->setSubject('タイトル')
-        ->setText('本文')
+        ->addTo($addTo)
+        ->setFrom('from@example.com')
+        ->setSubject($setSubject)
+        ->setText($setText)
     ;
 
     $result = $sendgrid->send($mail);
+}
+
 ?>
